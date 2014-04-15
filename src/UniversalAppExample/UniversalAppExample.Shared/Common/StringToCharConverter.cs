1c50211a8f7c329947999c2e3ae6a31d253fcbfe
Srđan Božović <srdjan.bozovic@live.com>
2014-04-15 22:14:30 +0200

    mod: universal windows 8 app shows magnitude

---
+++ src/UniversalAppExample/UniversalAppExample.Shared/Common/StringToCharConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace UniversalAppExample.Common
+{
+    public class StringToCharConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var index = Int32.Parse((string)parameter);
+            var text = (string)value;
+            if (index >= text.Length)
+                return ' ';
+            return text[index];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return true;
+        }
+    }
+}
